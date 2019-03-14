6f82cd6fac932ad7fcdd3e1a191c96a44964a483
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-14 15:14:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(input)
+  words = []
+  if ((input >= 3) && (input % 3 == 0))
+    words.push("Fizz")
+  end
+
+  if ((input >= 5) && (input % 5 == 0))
+    words.push("Buzz")
+  end
+
+  if (words.length == 0)
+    nil
+  else
+    words.join
+  end
+end
