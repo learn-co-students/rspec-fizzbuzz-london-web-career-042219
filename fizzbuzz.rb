6772f47fb77faca3cd8e8decf8a0da422f100582
Aiden leeming <aidenjl193@gmail.com>
2019-04-04 12:51:07 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  str = ""
+  str += num % 3 == 0 ? "Fizz" : ""
+  str += num % 5 == 0 ? "Buzz" : ""
+  
+  return str == "" ? nil : str
+end
